a8a86146c831e9de937be602fdc865fb6a6394a7
Hajime Nakagami <nakagami@gmail.com>
2022-05-02 09:46:30 +0900

    add deps

--- mix.exs
+++ mix.exs
@@ -1,12 +1,15 @@
 defmodule EctoFirebird.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :ecto_firebird,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      docs: docs(),
       deps: deps()
     ]
   end
@@ -18,11 +21,22 @@ defmodule EctoFirebird.MixProject do
     ]
   end
 
+  defp docs() do
+    [
+      source_ref: "v#{@version}",
+      main: "readme",
+      extras: ["README.md"]
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ecto_sql, "~> 3.7"},
+      {:ecto, "~> 3.7"},
+      {:firebirdex, "~> 0.2"},
     ]
   end
 end
