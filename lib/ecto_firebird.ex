47d3411e29f7eada0db71d084dd166258f74da3b
Hajime Nakagami <nakagami@gmail.com>
2022-05-02 09:25:33 +0900

    init project

---
+++ lib/ecto_firebird.ex
@@ -0,0 +1,18 @@
+defmodule EctoFirebird do
+  @moduledoc """
+  Documentation for EctoFirebird.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoFirebird.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_firebird_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoFirebirdTest do
+  use ExUnit.Case
+  doctest EctoFirebird
+
+  test "greets the world" do
+    assert EctoFirebird.hello() == :world
+  end
+end
